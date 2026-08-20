7eff867ed1383f0e133484a730fc5948cf3cb9b7
Phithakphon Chayanonnurak <chayanonnurak_p@silpakorn.edu>
2026-08-20 04:51:25 +0000

    first react app

---
+++ yawnian-app/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from './Components/Header';
+import MovieList from './Components/MovieList';
+import Footer from './Components/Footer';
+import Greeting from './Components/Greeting';
+import Card from './Components/Card';
+
+const students = [
+  { id: 1, name: 'ฝน',   year: 3 },
+  { id: 2, name: 'เต้ย', year: 2 },
+  { id: 3, name: 'มายด์', year: 4 },
+];
+
+function App() {
+  return (
+    <div>
+      <Card title="ประกาศ">
+     <p>สัปดาห์หน้าเรียนเรื่อง <b>State & Forms</b></p>
+     <p>อย่าลืม push การบ้านขึ้น GitHub!</p>
+     </Card>
+      <h1>รายชื่อนักศึกษา</h1>
+      {students.map((s) => (
+        <Greeting key={s.id} name={s.name} year={s.year} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ yawnian-app/src/Components/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello() {
+  return <h2>สวัสดีจาก Component แรกของYawnian! 🎉</h2>;
+}
+
+export default Hello;
