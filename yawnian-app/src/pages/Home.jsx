905014882db03e2c2604ccc9ec168512a49e26cc
Phithakphon Chayanonnurak <chayanonnurak_p@silpakorn.edu>
2026-09-10 05:30:36 +0000

    newmovie

--- yawnian-app/src/pages/Home.jsx
+++ yawnian-app/src/pages/Home.jsx
@@ -1,8 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { movies } from "../data";
+import MovieCard from "../Components/MovieCard";
+import MovieGallery from "../Components/MovieGallery";
+
 function Home() {
   return (
     <div className="p-8 text-center">
       <h1 className="text-3xl font-bold text-slate-800">🏠 หน้าแรก</h1>
       <p className="mt-2 text-slate-500">ยินดีต้อนรับสู่ MovieHub</p>
+      <h2 className="mt-8 text-2xl form">
+      หนังแนะนำ
+      </h2>
+      <div className="mx-auto mt-8 max-w-5xl">
+  {movies.slice(0, 3).map((movie) => (
+    <MovieCard
+      key={movie.id}
+      title={movie.title}
+      year={movie.year}
+    />
+  ))}
+  <h2 className="mt-8 text-2xl font-bold text-slate-800">ดูหนังเพิ่มเติม
+      <div className="mt-6">
+    <MovieGallery />
+  </div>
+  </h2>
+</div>
     </div>
   );
 }
